56b7b2a0f3cadba76384bb96830e12c3570e54a3
André (ugsto) Bortoli <andre.ugsto@proton.me>
2023-12-06 11:53:45 -0300

    refactor: adjust project structure

--- app/src/third-parties/communishield/errors/invalid-request.error.ts
+++ app/src/third-parties/communishield/errors/invalid-request.error.ts
@@ -1,7 +1,7 @@
 import { CommunishieldError } from './communishield';
 
 export class InvalidRequestError extends CommunishieldError {
-	constructor(reason: string) {
-		super(`Invalid request: ${reason.message}`);
-	}
+  constructor(reason: string) {
+    super(`Invalid request: ${(reason as any).message}`);
+  }
 }
